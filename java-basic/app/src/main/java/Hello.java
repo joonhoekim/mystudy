28596e5cc005021a23802599c566f8f0a6b45922
Joonhoe Kim <joonhoekim@Joonhoes-MacBook-Air.local>
2023-11-20 18:05:09 +0900

    gradle added

--- java-basic/app/src/main/java/Hello.java
+++ java-basic/app/src/main/java/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello, World");
+        System.out.print("안녕하세요!");
     }
 }
---
+++ java-basic/app/src/main/java/Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+  public static void main(String args) {
+    // JVM을 사용하여 이 클래스를 실행하면 바로 이 블럭 안에 있는 자바 코드가 실행된다.
+    System.out.println(100);
+    System.out.println(3.14);
+    System.out.println("한글입니당.");
+    System.out.println("abc");
+    System.out.println(true);
+    System.out.println(false);
+  }
+}
