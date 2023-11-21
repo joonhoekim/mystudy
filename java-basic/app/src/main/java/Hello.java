0834c95165a2407f0dfa01f4ee99d3e0bd09bcde
Joonhoe Kim <joonhoekim@Joonhoes-MacBook-Air.local>
2023-11-21 18:27:55 +0900

    ready for eclipse

--- java-basic/app/src/main/java/Hello.java
+++ java-basic/app/src/main/java/Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
-    public static void main(String[] args) {
-        System.out.print("Hello, World");
-        System.out.print("안녕하세요!");
-    }
+  public static void main(String[] args) {
+    System.out.print("Hello, World");
+    System.out.print("안녕하세요!");
+    System.out.println("HiHi");
+  }
 }
+// 저장할 때 javac로 컴파일되며 Run 하면 java 명령어를 실행한다.
+// app/bin/ 폴더가 생긴다. 이건 이클립스가 만든거다.
+// 저장할 때 이클립스가 소스를 클래스파일로 컴파일한 것을 bin 폴더에 저장한다.
+// app/build/ 는 gradle에서 만든거고.
--- java-basic/app/src/main/java/com/eomcs/F.java
+++ java-basic/app/src/main/java/com/eomcs/F.java
@@ -1,7 +1,6 @@
 package com.eomcs;
 
 class F1{
-  F1 v1; //필드 선언
-  F2 v2;
+
 }
 class F2{}
