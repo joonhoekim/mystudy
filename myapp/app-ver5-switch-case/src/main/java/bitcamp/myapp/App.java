aa4f02c3c71ac94949dc344d8bcd10f6b6f85de5
JoohoeKim <26rote@gmail.com>
2023-11-29 16:34:07 +0900

    리팩토링: 배열을 사용해보기

--- myapp/app-ver5-switch-case/src/main/java/bitcamp/myapp/App.java
+++ myapp/app-ver5-switch-case/src/main/java/bitcamp/myapp/App.java
@@ -17,21 +17,23 @@ public class App {
     final String ANSI_BOLD_RED = "\033[1;31m";
     final String ANSI_RED = "\033[31m";
 
-    //배열을 활용해보자.
-    String[] menuStrings = {
-        "--------------------------------",
-        ANSI_BOLD_RED + "[과제관리 시스템]\n" + ANSI_CLEAR,
-        "1. 과제",
-        "2. 게시글",
-        "3. 도움말",
-        "4. 종료",
-        "--------------------------------",
-    };
-
-    //배열을 활용해 메뉴를 출력해보자.
-    for (String str : menuStrings) {
-      System.out.println(str);
-    }
+    String appTitle = "[과제관리 시스템]\n";
+    String line1 = "--------------------------------";
+    String menu1 = "1. 과제",
+        menu2 = "2. 게시글",
+        menu3 = "3. 도움말",
+        menu4 = "4. 종료";
+    //이처럼 변수를 활용하면 메모리를 사용하므로 성능상은 아주 조금의 손해가 있을 수 있다.
+    //그러나 하드웨어 성능은 겁나게 좋아지고 있고, 개발자 인건비는 겁나게 늘어나고 있다.
+    //그래서 Refactoring에서 가장 중요한 것 중 하나가 가독성 향상 + 샏산성 향상이다.
+    System.out.println(line1);
+    System.out.println(ANSI_BOLD_RED + appTitle + ANSI_CLEAR);
+    // \n = LF = 0xOA in UTF-8
+    System.out.println(menu1);
+    System.out.println(menu2);
+    System.out.println(menu3);
+    System.out.println(ANSI_RED + menu4 + ANSI_CLEAR);
+    System.out.println(line1);
 
     String menuString;
 
@@ -54,9 +56,13 @@ public class App {
           System.out.println("종료입니다.");
           break loop;
         case "menu": {
-          for (String str : menuStrings) {
-            System.out.println(str);
-          }
+          System.out.println(line1);
+          System.out.println(ANSI_BOLD_RED + appTitle + ANSI_CLEAR);
+          System.out.println(menu1);
+          System.out.println(menu2);
+          System.out.println(menu3);
+          System.out.println(ANSI_RED + menu4 + ANSI_CLEAR);
+          System.out.println(line1);
           break;
         }
         default:
