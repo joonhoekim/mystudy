e4e8c712d2416f2becb6c0595610382916574afd
JoohoeKim <26rote@gmail.com>
2023-11-29 15:37:46 +0900

    (String & int both) used

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class App {
 
+  //java.util.Scanner inSt = new Scanner(System.in);
+
 
   public static void main(String[] args) {
     InputStream in = System.in;
@@ -24,7 +26,6 @@ public class App {
     //이처럼 변수를 활용하면 메모리를 사용하므로 성능상은 아주 조금의 손해가 있을 수 있다.
     //그러나 하드웨어 성능은 겁나게 좋아지고 있고, 개발자 인건비는 겁나게 늘어나고 있다.
     //그래서 Refactoring에서 가장 중요한 것 중 하나가 가독성 향상 + 샏산성 향상이다.
-
     System.out.println(line1);
     System.out.println(ANSI_BOLD_RED + appTitle + ANSI_CLEAR);
     // \n = LF = 0xOA in UTF-8
@@ -34,27 +35,40 @@ public class App {
     System.out.println(ANSI_RED + menu4 + ANSI_CLEAR);
     System.out.println(line1);
 
+    String menuString;
     int menuNumber = 0;
-    while (menuNumber != 4) {
+
+    for (; ; ) {
+      //여기서 변수를 생성해도 JVM이 똑똑해서 변수를 반복할 때마다 만들지는 않는다.
       System.out.print("> ");
-      menuNumber = keyScan.nextInt();
-      //keyScan.nextLine();
-      switch (menuNumber) {
-        case 1:
+      menuString = keyScan.nextLine();
+
+      if (menuString.equals("menu")) {
+        System.out.println(line1);
+        System.out.println(ANSI_BOLD_RED + appTitle + ANSI_CLEAR);
+        // \n = LF = 0xOA in UTF-8
+        System.out.println(menu1);
+        System.out.println(menu2);
+        System.out.println(menu3);
+        System.out.println(ANSI_RED + menu4 + ANSI_CLEAR);
+        System.out.println(line1);
+      } else {
+        menuNumber = Integer.valueOf(menuString);
+
+        if (menuNumber == 1) {
           System.out.println("과제입니다.");
-          break;
-        case 2:
+        } else if (menuNumber == 2) {
           System.out.println("게시글입니다.");
-          break;
-        case 3:
+        } else if (menuNumber == 3) {
           System.out.println("도움말입니다.");
+        } else if (menuNumber == 4) {
+          System.out.println("종료입니다.");
           break;
-        case 4:
-          System.out.println("종료합니다.");
-          break;
-        default:
-          System.out.println("알맞은 메뉴 번호를 입력해주세요.");
+        } else {
+          System.out.println("옳지 않은 번호입니다.");
+        }
       }
     }
+    keyScan.close(); //close가 있는 도구(메서드)도 있고 없는 것도 있다.
   }
 }
