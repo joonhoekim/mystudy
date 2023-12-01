c900c9c41360b85ffbcb3e78ca4c9ffab8782fd2
JoohoeKim <26rote@gmail.com>
2023-12-01 14:22:37 +0900

    method study ver2

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -31,8 +31,7 @@ public class App {
           onAssignment(keyIn);
           break;
         case "2":
-          System.out.println("게시글입니다.");
-          break;
+          onBoard(keyIn);
         case "3":
           System.out.println("도움말입니다.");
           break;
@@ -110,6 +109,43 @@ public class App {
           System.out.println("메뉴 번호가 옳지 않습니다!");
       }
     }
+  }
+
+  static void onBoard(Scanner keyIn) {
+    printBoardMenu();
+
+    while (true) {
+      String input = prompt("메뉴 > 게시글", keyIn);
+      switch (input) {
+        case "1":
+          System.out.println("등록입니다.");
+          break;
+        case "2":
+          System.out.println("조회입니다.");
+          break;
+        case "3":
+          System.out.println("변경입니다.");
+          break;
+        case "4":
+          System.out.println("삭제입니다.");
+        case "0":
+          return;
+        case "menu":
+          printBoardMenu();
+          break;
+        default:
+          System.out.println("메뉴 번호가 옳지 않습니다!");
+      }
+    }
+  }
+
+  static void printBoardMenu() {
+    System.out.println("[게시글]");
+    System.out.println("1. 등록");
+    System.out.println("2. 조회");
+    System.out.println("3. 변경");
+    System.out.println("4. 삭제");
+    System.out.println("0. 이전");
   }
 
 }
