27c26e803fc65d7f4c568f3a673ef1c5a1ca2474
JoonhoeKim <26rote@gmail.com>
2023-12-27 10:41:04 +0900

    LinkedList Study

--- myapp/app/src/main/java/bitcamp/util/LinkedList.java
+++ myapp/app/src/main/java/bitcamp/util/LinkedList.java
@@ -2,8 +2,9 @@ package bitcamp.util;
 
 public class LinkedList {
 
-  Node first;
-  Node last;
+  private Node first;
+  private Node last;
+  private int size;
 //  Node next;
 
   public void add(Object value) {
@@ -16,12 +17,39 @@ public class LinkedList {
     } else {
       //기존 노드객체 있을 때,
       //마지막 노드의 다음 노드로, 새로 만든 노드 인스턴스를 연결해준다.
-      last.next = node;
-      last = node;
+      this.last.next = node;
+      this.last = node;
+    }
+    ++size;
+  }
 
+  public Object[] toArray() {
+    Object[] arr = new Object[size];
+    Node node = first;
+//    for (int i = 0; i < size; ++i) {
+//      arr[i] = node.value;
+//      node = node.next;
+//    }
+    int index = 0;
+
+    while (node != null) {
+      arr[index++] = node.value;
+      node = node.next;
     }
 
+    return arr;
+  }
 
+  public Object get(int index) throws IndexOutOfBoundsException /*SKIP ABLE*/ {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException("Invalid Index"); //RuntimeException:Unchecked Exception.
+    }
+    int cursor = 0;
+    Node node = first;
+    while (cursor++ < index) {
+      node = node.next;
+    }
+    return node.value;
   }
 
 }
--- myapp/app/src/main/java/bitcamp/util/Test.java
+++ myapp/app/src/main/java/bitcamp/util/Test.java
@@ -2,17 +2,17 @@ package bitcamp.util;
 
 public class Test {
 
-  public static void main(String[] args) {
+  public static void main(String... args) {
     LinkedList list = new LinkedList();
-    list.add("aaa");
-    list.add("bbb");
-    list.add("ccc");
-    String[] test = new String[3];
-    System.out.println(list.first.value);
-    System.out.println(list.first.next.value);
-    System.out.println(list.first.next.next.value);
-    System.out.println(list.last.value);
+    list.add(new String("a"));
+    list.add(new String("B"));
+    list.add(new String("ca"));
+    list.add(new String("D"));
 
-
+    Object[] arr = list.toArray();
+    for (Object value : arr) {
+      System.out.println(value);
+    }
   }
+
 }
