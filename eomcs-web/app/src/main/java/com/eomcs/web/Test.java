0dfe377b486015e1e9895ce43293b7661e44e64e
Joonhoe Kim <26rote@gmail.com>
2024-04-18 11:55:47 +0900

    Study: CSS Study

---
+++ eomcs-web/app/src/main/java/com/eomcs/web/Test.java
@@ -0,0 +1,25 @@
+package com.eomcs.web;
+
+public class Test {
+//
+//  static class Box {
+//    int value;
+//  }
+//
+//  public static Box createBox() {
+//    int value = 100;
+//
+//    class Box {
+//      public int getValue() {
+//        return value;
+//      }
+//    }
+//    return new Box();
+//  }
+//
+//  public static void main(String[] args) {
+//    Box box = createBox();
+//    System.out.println(box.getValue());
+//  }
+
+}
